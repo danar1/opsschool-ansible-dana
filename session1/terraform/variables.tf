552e70f6c0de7513822665c91389eb7368632c41
Dana Regev <dregev@paloaltonetworks.com>
2020-10-31 09:24:24 +0200

    Update vpc and subnets ids

--- session1/terraform/variables.tf
+++ session1/terraform/variables.tf
@@ -9,12 +9,12 @@ variable "aws_region" {
 
 variable "vpc_id" {
   description = "AWS VPC id"
-  default     = "vpc-05c75f72d7837f8d0"
+  default     = "vpc-a0d417dd"
 }
 
 variable "subnet_id" {
   description = "Ansible Subnet id"
-  default     = "subnet-088d7d0c821319b3c"
+  default     = "subnet-403c8c1f"
 }
 
 variable "ingress_ports" {
