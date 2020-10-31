e49687ac733c2cbfe4fe83625e3a3a68afe11dea
danar1 <danar1@users.noreply.github.com>
2020-10-31 23:48:10 +0200

    Add ansible setup for server, add ansible setup for nodes - install docker [WIP]

--- session1/terraform/keys.tf
+++ session1/terraform/keys.tf
@@ -10,5 +10,6 @@ resource "aws_key_pair" "ansible_key" {
 
 resource "local_file" "ansible_key" {
   sensitive_content  = tls_private_key.ansible_key.private_key_pem
+  file_permission    = "0600"
   filename           = "ansible_key.pem"
 }
